ebd8fa2fa4c4853dac52e66df5fc568a30fbd587
Brian Durand <bbdurand@gmail.com>
2020-06-11 00:23:47 -0700

    fix server middleware to read from job encrypted args option

--- lib/sidekiq/encrypted_args/server_middleware.rb
+++ lib/sidekiq/encrypted_args/server_middleware.rb
@@ -3,11 +3,11 @@
 module Sidekiq
   module EncryptedArgs
     class ServerMiddleware
-      # Sidekiq server middleware for encrypting arguments on jobs for workers
-      # with `encrypted_args` set in the `sidekiq_options`.
+      # Sidekiq server middleware for decrypting arguments on jobs that have encrypted args.
       def call(worker, job, queue)
-        encrypted_args = EncryptedArgs.send(:encrypted_args_option, worker.class.sidekiq_options, worker.class, job["args"])
+        encrypted_args = job["encrypted_args"]
         if encrypted_args
+          encrypted_args = backward_compantible_encrypted_args(encrypted_args, worker.class, job)
           job_args = job["args"]
           encrypted_args.each do |position|
             value = job_args[position]
@@ -16,6 +16,17 @@ module Sidekiq
         end
 
         yield
+      end
+
+      private
+
+      # Ensure that the encrypted args is an array of integers. If not re-read it from the class
+      # definition since gem version 1.0.2 and earlier did not update the encrypted_args on the job.
+      def backward_compantible_encrypted_args(encrypted_args, worker_class, job)
+        unless encrypted_args.is_a?(Array) && encrypted_args.all? { |position| position.is_a?(Integer) }
+          encrypted_args = EncryptedArgs.send(:encrypted_args_option, encrypted_args, worker_class, job["args"])
+        end
+        encrypted_args
       end
     end
   end
